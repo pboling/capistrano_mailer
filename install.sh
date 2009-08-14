109c5667cbca37d3f86c825ed0797e2f04b47787
pboling <peter.boling@peterboling.com>
2009-08-14 12:14:17 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 # I use this to make life easier when installing and testing from source:
-rm -rf capistrano_mailer-0.2.0.gem && gem build capistrano_mailer.gemspec && sudo gem uninstall capistrano_mailer && sudo gem install capistrano_mailer-0.2.0.gem
+rm -rf capistrano_mailer-*.gem && gem build capistrano_mailer.gemspec && sudo gem uninstall capistrano_mailer && sudo gem install capistrano_mailer-0.2.0.gem --no-ri --no-rdoc
