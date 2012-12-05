f046eff0bc78a12bfb151e4170de818bffc81cde
Mark Sim <marksim@gmail.com>
2012-12-05 12:50:06 -0600

    Set Release Notes

--- lib/capistrano/mailer_recipes.rb
+++ lib/capistrano/mailer_recipes.rb
@@ -8,6 +8,12 @@ namespace :deploy do
   desc "Send email notification of deployment (only send variables you want to be in the email)"
   task :notify, :roles => :app do
     show.me  # this sets the task_name variable
+    
+    # Set the release notes
+    git_commits_range = "#{latest_revision.strip}..#{current_revision.strip}"
+    git_log = `git log --pretty=oneline --abbrev-commit #{git_commits_range}` # executes in local shell
+    set :release_notes, git_log.blank? ? "No Changes since last deploy." : git_log
+    
     mailer.send_notification_email(self)
   end
 end
