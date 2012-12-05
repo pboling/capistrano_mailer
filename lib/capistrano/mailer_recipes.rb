6d8ea43a8adda92cc4d8498e3f51062de373fb33
Mark Sim <marksim@gmail.com>
2012-12-05 13:03:46 -0600

    Add initial break for any release notes based on git log

--- lib/capistrano/mailer_recipes.rb
+++ lib/capistrano/mailer_recipes.rb
@@ -12,7 +12,7 @@ namespace :deploy do
     # Set the release notes
     git_commits_range = "#{latest_revision.strip}..#{current_revision.strip}"
     git_log = `git log --pretty=oneline --abbrev-commit #{git_commits_range}` # executes in local shell
-    set :release_notes, git_log.blank? ? "No Changes since last deploy." : git_log
+    set :release_notes, git_log.blank? ? "No Changes since last deploy." : "\n" + git_log
     
     mailer.send_notification_email(self)
   end
